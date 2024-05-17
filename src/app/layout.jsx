d434d9b48ee3d8d8a126639240752d1ec9530862
CodexCoding2000 <codexcoding99@gmail.com>
2024-05-17 13:26:23 +0700

    move link to document file

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,12 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Athiti&display=swap" rel="stylesheet" />
-        <link href="https://kit-pro.fontawesome.com/releases/v6.5.1/css/pro.min.css" rel="stylesheet" />
-      </head>
       <body className="overflow-x-hidden">
         <SessionProvider>
           {children}
