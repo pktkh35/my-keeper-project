b1128ee53958486f64b13f30f0355ff98ffabb4d
pktkh35 <codexcoding99@gmail.com>
2026-05-27 11:42:35 +0700

    remove google login

--- src/app/login/(page)/Login.jsx
+++ src/app/login/(page)/Login.jsx
@@ -20,12 +20,12 @@ const Login = () => {
             <p className="text-sm opacity-60 text-white">
                 ให้เราช่วยคุณจัดการรายการสิ่งที่ต้องทำเพื่อให้เป็นระบบระเบียบและตรวจสอบง่ายมากขึ้น
             </p>
-            <Button className="w-full mt-2" onClick={() => signIn("google", {
+            {/* <Button className="w-full mt-2" onClick={() => signIn("google", {
                 callbackUrl: "/",
                 redirect: "/"
             })}>
                 เข้าสู่ระบบด้วย Google
-            </Button>
+            </Button> */}
             <Button className="w-full mt-2" onClick={() => signIn("discord", {
                 callbackUrl: "/",
                 redirect: "/"
